317066552cb95cbb4f5675988c51d9aedf5be388
SaxenaBhoomika <89317642+SaxenaBhoomika@users.noreply.github.com>
2023-06-04 16:41:23 +0530

    Create numberOfProvinces.cpp

---
+++ numberOfProvinces.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void dfs(int s,int n,vector<vector<int>>& isConnected,vector<bool>&visited){
+        visited[s]=true;
+        vector<int>adj;
+        for(int i=0;i<n;i++){
+            int x=isConnected[s][i];
+            if(x==1){
+                adj.push_back(i);
+            }
+        }
+        for(auto x:adj){
+            if(!visited[x]){
+                dfs(x,n,isConnected,visited);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<bool>visited(n,0);
+        int res=0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                res++;
+                dfs(i,n,isConnected,visited);
+            }
+        }
+        return res;
+    }
+};
