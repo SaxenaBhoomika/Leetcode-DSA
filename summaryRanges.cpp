359a4f8fcb3552b172eedbc8b10a123eb10bb4ef
SaxenaBhoomika <89317642+SaxenaBhoomika@users.noreply.github.com>
2023-06-12 13:52:05 +0530

    Create summaryRanges.cpp

---
+++ summaryRanges.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int n=nums.size();
+        vector<string>res;
+        string ans="";
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(j+1<n && nums[j+1]==nums[j]+1){
+                j++;
+            }
+            if(j>i){
+                ans+=to_string(nums[i]);
+                ans+="->";
+                ans+=to_string(nums[j]);
+            }
+            else{
+                ans+=to_string(nums[i]);
+            }
+            res.push_back(ans);
+            ans="";
+            i=j;
+        }
+        return res;
+    }
+
+};
