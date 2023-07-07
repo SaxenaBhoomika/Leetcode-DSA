22cabe1863a6b3d3f7db5b5af34232bbf33306bb
SaxenaBhoomika <89317642+SaxenaBhoomika@users.noreply.github.com>
2023-07-07 21:29:23 +0530

    Create maximizeTheConfusionOfExam.cpp

---
+++ maximizeTheConfusionOfExam.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        int n=answerKey.size();
+        int t=0,f=0;
+        int start=0,end=0;
+        int res=0;
+        while(end<n){
+            t+=(answerKey[end]=='T');
+            f+=(answerKey[end]=='F');
+            while(t>k && f>k){
+                if(answerKey[start]=='T'){
+                    t--;
+                }
+                else{
+                    f--;
+                }
+                start++;
+            }
+
+            res=max(res,end-start+1);
+            end++;
+        }
+        return res;
+
+    }
+};
